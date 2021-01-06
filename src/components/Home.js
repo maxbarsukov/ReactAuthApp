cd0c8477f28ca6f97f8129452f32dddfcae9d10f
maxbarsukov <maximbarsukov@bk.ru>
2021-01-06 12:11:04 +0300

    some style changes at home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Hello</h1>
+                <h1>Home</h1>
                 <h1>Status: {this.props.loggedInStatus}</h1>
-                <Registration/>
+                    <Registration/>
             </div>
         );
     }
