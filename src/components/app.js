3fa2eb6c3cae4c8d64b703ae88a6e773826ffc25
maxbarsukov <maximbarsukov@bk.ru>
2021-01-06 11:02:06 +0300

    components added

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouting, Switch, Route } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <BrowserRouting>
+          <Switch>
+            <Route exact path={"/"} component={Home}/>
+            <Route exact path={"/dashboard"} component={Dashboard}/>
+          </Switch>
+        </BrowserRouting>
       </div>
     );
   }
