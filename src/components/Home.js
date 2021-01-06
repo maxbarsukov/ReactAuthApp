0ee1e939c8be7de61d677a0387913d6fddf84492
maxbarsukov <maximbarsukov@bk.ru>
2021-01-06 12:20:24 +0300

    registration updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ export default class Home extends Component {
             <div>
                 <h1>Home</h1>
                 <h1>Status: {this.props.loggedInStatus}</h1>
-                <Registration/>
+                <Registration handleSuccessfulAuth={this.handleSuccessfulAuth} />
             </div>
         );
     }
