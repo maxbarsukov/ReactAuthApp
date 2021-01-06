60133468c9a37172b0b8a513b47c6f29e8c1fc7e
maxbarsukov <maximbarsukov@bk.ru>
2021-01-06 12:22:53 +0300

    little changes at app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import axios from "axios";
+
 import Home from "./Home";
 import Dashboard from "./Dashboard";
 
