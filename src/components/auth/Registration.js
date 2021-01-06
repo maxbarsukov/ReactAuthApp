a90cd6edcb683774ceb5c87cdfc034edb9670066
maxbarsukov <maximbarsukov@bk.ru>
2021-01-06 11:36:59 +0300

    reg form updated

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -28,7 +28,34 @@ export default class Registration extends Component {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleGhange} required/>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChange={this.handleGhange}
+                        required
+                    />
+
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleGhange}
+                        required
+                    />
+
+                    <input
+                        type="password_confirmation"
+                        name="password_confirmation"
+                        placeholder="Password confirmation"
+                        value={this.state.password_confirmation}
+                        onChange={this.handleGhange}
+                        required
+                    />
+
+                    <button>Register</button>
                 </form>
             </div>
         );
