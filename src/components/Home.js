5f097acc298a1dc97b2ad3bf37a11e98605d4d5b
maxbarsukov <maximbarsukov@bk.ru>
2021-01-06 12:03:50 +0300

    registration to home page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Registration from "./auth/Registration";
 
 export default class Home extends Component {
     render() {
         return (
             <div>
                 <h1>Hello</h1>
-                <h1>Hello</h1>
+                <Registration/>
             </div>
         );
     }
