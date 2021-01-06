7869649d67cd62c8ed879dc523887dcf1f5edec0
maxbarsukov <maximbarsukov@bk.ru>
2021-01-06 12:17:28 +0300

    constructor added to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import Registration from "./auth/Registration";
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleSuccessfulAuth = this.handleSuccessfulAuth.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    }
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
                 <h1>Status: {this.props.loggedInStatus}</h1>
-                    <Registration/>
+                <Registration/>
             </div>
         );
     }
