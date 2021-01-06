bbfea5e622774290967179fae9be91fd5eabd7f8
maxbarsukov <maximbarsukov@bk.ru>
2021-01-06 11:39:58 +0300

    axios added to reg

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class Registration extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            password: "",
-            password_confirmation: "",
-            registerErrors: ""
-        }
+  constructor(props) {
+    super(props);
 
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
+    this.state = {
+      email: "",
+      password: "",
+      password_confirmation: "",
+      registrationErrors: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
     handleSubmit(event) {
         console.log("form submitted");
@@ -25,39 +27,39 @@ export default class Registration extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        value={this.state.email}
-                        onChange={this.handleGhange}
-                        required
-                    />
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
 
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        value={this.state.password}
-                        onChange={this.handleGhange}
-                        required
-                    />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
 
-                    <input
-                        type="password_confirmation"
-                        name="password_confirmation"
-                        placeholder="Password confirmation"
-                        value={this.state.password_confirmation}
-                        onChange={this.handleGhange}
-                        required
-                    />
+          <input
+            type="password"
+            name="password_confirmation"
+            placeholder="Password confirmation"
+            value={this.state.password_confirmation}
+            onChange={this.handleChange}
+            required
+          />
 
-                    <button>Register</button>
-                </form>
-            </div>
-        );
-    }
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    );
+  }
 }
