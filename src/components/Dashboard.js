28cb70872268dd586187ff6b905b7f001a51518f
maxbarsukov <maximbarsukov@bk.ru>
2021-01-06 12:16:36 +0300

    some changes at dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Dashboard = () => {
-    return(
-        <div>
-            <div>
-                <h1>Dashboard</h1>
-                <h1>Status: {this.props.loggedInStatus}</h1>
-            </div>
-        </div>
-    );
-}
+const Dashboard = props => {
+  return (
+    <div>
+      <div>
+        <h1>Dashboard</h1>
+        <h1>Status: {props.loggedInStatus}</h1>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
